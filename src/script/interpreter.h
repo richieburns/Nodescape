0e8e5b7d132333fa2faea1c719690d312f593844
richieburns <32524646+richieburns@users.noreply.github.com>
2018-09-14 15:08:39 +0100

    Remove redifined flags

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -21,9 +21,6 @@ class uint256;
 /** Signature hash types/flags */
 enum
 {
-    SIGHASH_ALL = 1,
-    SIGHASH_NONE = 2,
-    SIGHASH_SINGLE = 3,
     SIGHASH_ALL          = 1,
     SIGHASH_NONE         = 2,
     SIGHASH_SINGLE       = 3,
