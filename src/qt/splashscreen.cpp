9817126495658b5bab8a87a491739b03394c8a6a
richieburns <32524646+richieburns@users.noreply.github.com>
2018-11-09 18:18:56 +0000

    Defualt to mainnet for launch

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -53,7 +53,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     QString splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash";
     if(GetBoolArg("-regtest", false))
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
-    if(GetBoolArg("-testnet", true))
+    if(GetBoolArg("-testnet", false))
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
 
     QString font = QApplication::font().toString();
